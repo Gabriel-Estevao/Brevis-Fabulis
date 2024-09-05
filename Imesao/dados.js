686155ffd16043bcc972f772b298d8f45c097f3d
Gabriel Cunha <106206308+Gabriel-Estevao@users.noreply.github.com>
2024-09-05 12:13:31 -0300

    Add files via upload

---
+++ Imesao/dados.js
@@ -0,0 +1,58 @@
+let dados = [
+    {
+        titulo : "Solo Leveling",
+        descricao : "Solo Leveling é um manhwa (quadrinho coreano) que narra a jornada extraordinária de Sung Jin-woo, um caçador de monstros incrivelmente fraco. Após um evento quase fatal em uma masmorra, Jin-woo desperta um sistema único que lhe permite evoluir e se tornar mais forte a uma taxa incomparável.",
+        link : "https://www.tappytoon.com/en/book/solo-leveling-official"
+    },
+    {
+        titulo : "Jujutsu Kaisen",
+        descricao : "Jujutsu Kaisen é um mangá que acompanha a jornada de Itadori Yuji, um estudante do ensino médio que, para salvar um amigo, ingere um dedo amaldiçoado e se torna um recipiente para o Rei das Maldições, Ryomen Sukuna. Agora, ele precisa aprender a controlar seus poderes e sobreviver no mundo oculto dos feiticeiros jujutsu.",
+        link : "https://www.mangaplus.shonenjump.com/pt-br/manga/jujutsu-kaisen"
+    },
+    {
+        titulo : "Tales of Demons and Gods",
+        descricao : "Tales of Demons and Gods é um xianxia que narra a história de Nie Li, um cultivador poderoso que reencarna em um mundo que ele ajudou a destruir em uma vida passada. Agora, ele precisa proteger seus amigos e familiares, enquanto busca vingança contra aqueles que o traíram.",
+        link : "https://www.webnovel.com/book/tales-of-demons-and-gods_10043793805889605"
+    },
+    {
+        titulo: "Attack on Titan",
+        sinopse: "Em um mundo cercado por muralhas gigantes, a humanidade vive em constante medo dos Titãs, criaturas gigantescas que devoram humanos. Eren Yeager jura exterminar todos os Titãs após presenciar a morte de sua mãe.",
+        link: "https://attack-on-titan.fandom.com/pt-br/wiki/Attack_on_Titan"
+    },
+    {
+        titulo: "One Piece",
+        sinopse: "Monkey D. Luffy, um jovem sonhador, embarca em uma grande aventura para encontrar o One Piece, o maior tesouro do mundo e se tornar o Rei dos Piratas.",
+        link: "https://onepiece.fandom.com/pt-br/wiki/One_Piece"
+    },
+    {
+        titulo: "Tower of God",
+        sinopse: "Um garoto chamado Bam, que viveu toda a sua vida isolado em uma torre, decide desafiar a torre e encontrar sua amiga Rachel.",
+        link: "https://www.webtoon.com/en/fantasy/tower-of-god"
+    },
+    {
+        titulo: "Naruto",
+        sinopse: "Naruto Uzumaki é um jovem ninja que busca reconhecimento e sonha em se tornar Hokage, o líder da sua vila. Ele embarca em uma jornada épica repleta de batalhas e amizades.",
+        link: "https://naruto.fandom.com/pt-br/wiki/Naruto"
+    },
+    {
+        titulo: "Bleach",
+        sinopse: "Ichigo Kurosaki, um adolescente comum, se torna um Shinigami substituto após um encontro com Rukia Kuchiki, uma Shinigami. Ele agora precisa proteger o mundo humano dos Hollows.",
+        link: "https://bleach.fandom.com/pt-br/wiki/Bleach"
+    },
+    {
+        titulo: "Demon Slayer",
+        sinopse: "Tanjiro Kamado, um jovem carvoeiro, se transforma em um caçador de demônios após sua família ser massacrada e sua irmã mais nova se tornar um demônio.",
+        link: "https://kimetsu-no-yaiba.fandom.com/pt-br/wiki/Kimetsu_no_Yaiba"
+    },
+    {
+        titulo: "My Hero Academia",
+        sinopse: "Em um mundo onde a maioria das pessoas possui superpoderes, Izuku Midoriya sonha em se tornar um herói, apesar de não ter nenhuma habilidade. Sua vida muda quando ele conhece o maior herói de todos, All Might.",
+        link: "https://myheroacademia.fandom.com/pt-br/wiki/My_Hero_Academia"
+    },
+    {
+        titulo: "Fullmetal Alchemist",
+        sinopse: "Os irmãos Edward e Alphonse Elric tentam trazer sua mãe de volta à vida através da alquimia, mas pagam um preço alto por isso. Eles embarcam em uma jornada para recuperar seus corpos perdidos.",
+        link: "https://fullmetal-alchemist.fandom.com/pt-br/wiki/Fullmetal_Alchemist"
+    },
+];
+
