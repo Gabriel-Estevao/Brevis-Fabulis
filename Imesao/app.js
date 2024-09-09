37fcac6395a28ba95759b7dc26ff5a23ee7e4b33
Gabriel Cunha <106206308+Gabriel-Estevao@users.noreply.github.com>
2024-09-09 17:45:18 -0300

    Add files via upload

--- Imesao/app.js
+++ Imesao/app.js
@@ -1,1 +1,52 @@
-console.log(dados);
+function pesquisar(){
+    
+let section = document.getElementById("resultados-pesquisa")
+console.log(section);
+
+let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+if (campoPesquisa == "") {
+    section.innerHTML = "<p> Nada foi  encontrado</p>"
+    return
+}
+
+campoPesquisa = campoPesquisa.toLowerCase()
+
+let resultados = "";
+let titulo = "";
+let sinopse = "";
+let genero = "";
+
+
+// Para cada dado dentro da lista da dados
+for (let dado of dados) {
+    titulo = dado.titulo.toLowerCase()
+    sinopse = dado.sinopse.toLowerCase()
+    genero = dado.genero.toLowerCase()
+    // se titulo includes campoPesquisa
+
+    if (titulo.includes(campoPesquisa) || sinopse.includes(campoPesquisa) || genero.includes(campoPesquisa)) {
+
+        // cria um novo elemento
+
+        resultados += `
+        <div class="item-resultado">
+                <h2>
+                    <a href="#" target="_blank">${dado.titulo}</a>
+                </h2>
+                <p class="descricao-meta">${dado.sinopse}</p>
+                <a href="${dado.link}" target="_blank">Mais informações</a>
+        </div>
+    `
+    }
+   
+}
+
+if (!resultados) {
+    resultados = "<p>Nada foi encontrado</p>"
+}
+
+section.innerHTML = resultados
+}
+
+// console.log(dados);
--- Imesao/dados.js
+++ Imesao/dados.js
@@ -1,58 +1,117 @@
 let dados = [
     {
         titulo : "Solo Leveling",
-        descricao : "Solo Leveling é um manhwa (quadrinho coreano) que narra a jornada extraordinária de Sung Jin-woo, um caçador de monstros incrivelmente fraco. Após um evento quase fatal em uma masmorra, Jin-woo desperta um sistema único que lhe permite evoluir e se tornar mais forte a uma taxa incomparável.",
-        link : "https://www.tappytoon.com/en/book/solo-leveling-official"
+        sinopse : "Solo Leveling é um manhwa (quadrinho coreano) que narra a jornada extraordinária de Sung Jin-woo, um caçador de monstros incrivelmente fraco. Após um evento quase fatal em uma masmorra, Jin-woo desperta um sistema único que lhe permite evoluir e se tornar mais forte a uma taxa incomparável.",
+        link : "https://www.tappytoon.com/en/book/solo-leveling-official",
+        genero: "manhwa"
     },
     {
         titulo : "Jujutsu Kaisen",
-        descricao : "Jujutsu Kaisen é um mangá que acompanha a jornada de Itadori Yuji, um estudante do ensino médio que, para salvar um amigo, ingere um dedo amaldiçoado e se torna um recipiente para o Rei das Maldições, Ryomen Sukuna. Agora, ele precisa aprender a controlar seus poderes e sobreviver no mundo oculto dos feiticeiros jujutsu.",
-        link : "https://www.mangaplus.shonenjump.com/pt-br/manga/jujutsu-kaisen"
+        sinopse : "Jujutsu Kaisen é um mangá que acompanha a jornada de Itadori Yuji, um estudante do ensino médio que, para salvar um amigo, ingere um dedo amaldiçoado e se torna um recipiente para o Rei das Maldições, Ryomen Sukuna. Agora, ele precisa aprender a controlar seus poderes e sobreviver no mundo oculto dos feiticeiros jujutsu.",
+        link : "https://www.mangaplus.shonenjump.com/pt-br/manga/jujutsu-kaisen",
+        genero: "manga"
     },
     {
         titulo : "Tales of Demons and Gods",
-        descricao : "Tales of Demons and Gods é um xianxia que narra a história de Nie Li, um cultivador poderoso que reencarna em um mundo que ele ajudou a destruir em uma vida passada. Agora, ele precisa proteger seus amigos e familiares, enquanto busca vingança contra aqueles que o traíram.",
-        link : "https://www.webnovel.com/book/tales-of-demons-and-gods_10043793805889605"
+        sinopse : "Tales of Demons and Gods é um xianxia que narra a história de Nie Li, um cultivador poderoso que reencarna em um mundo que ele ajudou a destruir em uma vida passada. Agora, ele precisa proteger seus amigos e familiares, enquanto busca vingança contra aqueles que o traíram.",
+        link : "https://www.webnovel.com/book/tales-of-demons-and-gods_10043793805889605",
+        genero: "Manhua"
     },
     {
         titulo: "Attack on Titan",
         sinopse: "Em um mundo cercado por muralhas gigantes, a humanidade vive em constante medo dos Titãs, criaturas gigantescas que devoram humanos. Eren Yeager jura exterminar todos os Titãs após presenciar a morte de sua mãe.",
-        link: "https://attack-on-titan.fandom.com/pt-br/wiki/Attack_on_Titan"
+        link: "https://attack-on-titan.fandom.com/pt-br/wiki/Attack_on_Titan",
+        genero: "manga"
     },
     {
         titulo: "One Piece",
         sinopse: "Monkey D. Luffy, um jovem sonhador, embarca em uma grande aventura para encontrar o One Piece, o maior tesouro do mundo e se tornar o Rei dos Piratas.",
-        link: "https://onepiece.fandom.com/pt-br/wiki/One_Piece"
+        link: "https://onepiece.fandom.com/pt-br/wiki/One_Piece",
+        genero: "manga"
     },
     {
         titulo: "Tower of God",
         sinopse: "Um garoto chamado Bam, que viveu toda a sua vida isolado em uma torre, decide desafiar a torre e encontrar sua amiga Rachel.",
-        link: "https://www.webtoon.com/en/fantasy/tower-of-god"
+        link: "https://www.webtoon.com/en/fantasy/tower-of-god",
+        genero: "manhwa"
     },
     {
         titulo: "Naruto",
         sinopse: "Naruto Uzumaki é um jovem ninja que busca reconhecimento e sonha em se tornar Hokage, o líder da sua vila. Ele embarca em uma jornada épica repleta de batalhas e amizades.",
-        link: "https://naruto.fandom.com/pt-br/wiki/Naruto"
+        link: "https://naruto.fandom.com/pt-br/wiki/Naruto",
+        genero: "manga"
     },
     {
         titulo: "Bleach",
         sinopse: "Ichigo Kurosaki, um adolescente comum, se torna um Shinigami substituto após um encontro com Rukia Kuchiki, uma Shinigami. Ele agora precisa proteger o mundo humano dos Hollows.",
-        link: "https://bleach.fandom.com/pt-br/wiki/Bleach"
+        link: "https://bleach.fandom.com/pt-br/wiki/Bleach",
+        genero: "manga"
     },
     {
         titulo: "Demon Slayer",
         sinopse: "Tanjiro Kamado, um jovem carvoeiro, se transforma em um caçador de demônios após sua família ser massacrada e sua irmã mais nova se tornar um demônio.",
-        link: "https://kimetsu-no-yaiba.fandom.com/pt-br/wiki/Kimetsu_no_Yaiba"
+        link: "https://kimetsu-no-yaiba.fandom.com/pt-br/wiki/Kimetsu_no_Yaiba",
+        genero: "manga"
     },
     {
         titulo: "My Hero Academia",
         sinopse: "Em um mundo onde a maioria das pessoas possui superpoderes, Izuku Midoriya sonha em se tornar um herói, apesar de não ter nenhuma habilidade. Sua vida muda quando ele conhece o maior herói de todos, All Might.",
-        link: "https://myheroacademia.fandom.com/pt-br/wiki/My_Hero_Academia"
+        link: "https://myheroacademia.fandom.com/pt-br/wiki/My_Hero_Academia",
+        genero: "manga"
     },
     {
         titulo: "Fullmetal Alchemist",
         sinopse: "Os irmãos Edward e Alphonse Elric tentam trazer sua mãe de volta à vida através da alquimia, mas pagam um preço alto por isso. Eles embarcam em uma jornada para recuperar seus corpos perdidos.",
-        link: "https://fullmetal-alchemist.fandom.com/pt-br/wiki/Fullmetal_Alchemist"
+        link: "https://fullmetal-alchemist.fandom.com/pt-br/wiki/Fullmetal_Alchemist",
+        genero: "manga"
     },
+    {
+        titulo: "I Shall Seal the Heavens",
+        sinopse: "Ye Chen, um gênio cultivador, reencarna em um mundo desconhecido e começa sua jornada para se tornar o mais forte.",
+        link: "https://www.webnovel.com/book/i-shall-seal-the-heavens_98054058054886304",
+        genero: "Manhua"
+    },
+    {
+        titulo: "Martial Peak",
+        sinopse: "Yang Kai, um jovem órfão, descobre um antigo livro de cultivo e começa sua jornada para se tornar o mais forte em um mundo dominado por artes marciais.",
+        link: "https://www.webnovel.com/book/martial-peak_97417058054886304",
+        genero: "Manhua"
+    },
+    {
+        titulo: "Peerless Martial God",
+        sinopse: "Lin Feng, um jovem talentoso, é traído e expulso de sua seita. Ele embarca em uma jornada de vingança e se torna um dos cultivadores mais poderosos do mundo.",
+        link: "https://www.webnovel.com/book/peerless-martial-god_97414058054886304",
+        genero: "manhwa"
+    },
+    {
+        titulo: "Lord of the Mysteries",
+        sinopse: "Em um mundo onde a magia e os mistérios coexistem, um jovem detetive se envolve em uma conspiração que ameaça a humanidade.",
+        link: "https://www.webnovel.com/book/lord-of-the-mysteries_120451058054886304",
+        genero: "manhwa"
+    },
+    {
+        titulo: "Release That Witch",
+        sinopse: "Em um mundo medieval com magia, um príncipe reencarnado usa seus conhecimentos modernos para desenvolver a tecnologia e mudar o reino.",
+        link: "https://www.webnovel.com/book/release-that-witch_105097058054886304",
+        genero: "manhwa"
+    },
+    {
+        titulo: "Against the Gods",
+        sinopse: "Yun Che, um jovem prodígio, é traído e expulso de sua seita. Ele busca vingança e se torna um dos cultivadores mais poderosos do mundo.",
+        link: "https://www.webnovel.com/book/against-the-gods_96980058054886304",
+        genero: "Manhua"
+    },
+    {
+        titulo: "Stellar Transformations",
+        sinopse: "Um jovem órfão descobre um antigo livro de cultivo e começa sua jornada para se tornar o mais forte em um mundo dominado por artes marciais e tecnologia espacial.",
+        link: "https://www.webnovel.com/book/stellar-transformations_96979058054886304",
+        genero: "Manhua"
+    },
+    {
+        titulo: "Swallowed Star",
+        sinopse: "Em um futuro onde a humanidade explora o espaço, um jovem se junta a uma organização militar para proteger a Terra e se tornar mais forte.",
+        link: "https://www.webnovel.com/book/swallowed-star_96978058054886304",
+        genero: "Manhua"
+    }
 ];
 
